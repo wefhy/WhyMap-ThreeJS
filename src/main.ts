a42d8d3996c15e7c3e5fec074835f5a0d567dfdf
wefhy <>
2023-07-26 20:40:32 +0200

    Implement 3d preview, fix analyze area

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import './style.css'
 
 import * as THREE from 'three';
 
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+// import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls';
 
 
 const scene = new THREE.Scene();
@@ -17,19 +18,17 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 camera.position.setZ(30);
+camera.position.setX(10);
+camera.position.setY(60);
+
 
 renderer.render(scene, camera);
-
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-// const material = new THREE.MeshBasicMaterial({color: 0xFF6347, wireframe: true});
-const material = new THREE.MeshStandardMaterial({color: 0xFF6347});
-const torus = new THREE.Mesh(geometry, material);
 
 // scene.add(torus);
 
 const ambientLight = new THREE.AmbientLight(0x444455);
-const pointLight = new THREE.PointLight(0xffffff);
-const hemiLight = new THREE.HemisphereLight( 0x0000ff, 0x00ff00, 0.6 );
+// const pointLight = new THREE.PointLight(0xffffff);
+// const hemiLight = new THREE.HemisphereLight( 0x0000ff, 0x00ff00, 0.6 );
 const sun = new THREE.DirectionalLight(0xffeedd, 0.8);
 sun.position.set(0, 100, 0)
 sun.target.position.set(-20, 0, -50);
@@ -45,7 +44,7 @@ sun.shadow.camera.top = 500;
 sun.shadow.camera.bottom = -500;
 
 
-pointLight.position.set(25, 25, 25);
+// pointLight.position.set(25, 25, 25);
 
 
 // scene.add(pointLight);
@@ -54,21 +53,20 @@ scene.add(ambientLight);
 scene.add(sun);
 scene.add(sun.target);
 
-const lightHelper = new THREE.PointLightHelper(pointLight);
+// const lightHelper = new THREE.PointLightHelper(pointLight);
 const gridHelper = new THREE.GridHelper(200, 50);
 
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 0, 0);
 
 
 
-scene.add(lightHelper, gridHelper);
+
+// scene.add(lightHelper);
+scene.add(gridHelper);
 
 function animate() {
     requestAnimationFrame(animate);
-
-    torus.rotation.x += 0.01;
-    torus.rotation.y += 0.005;
-    torus.rotation.z += 0.01;
 
     controls.update();
 
@@ -77,17 +75,18 @@ function animate() {
 
 animate();
 
-// const terrain = new THREE.Mesh(
-//     new THREE.PlaneGeometry(3, 3, 64, 64),
-//     new THREE.MeshStandardMaterial({color: 0x00ff00})
-// );
-//
-// terrain.rotation.x = -Math.PI / 2;
-// terrain.position.y = -0.5;
-//
-// scene.add(terrain);
-
 // const meshUrl = 'http://localhost:7542/three/tiles/17/65508/65544'
+
+const urlParams = new URLSearchParams(window.location.search);
+const x1 = urlParams.get('x1');
+const x2 = urlParams.get('x2');
+const z1 = urlParams.get('z1');
+const z2 = urlParams.get('z2');
+
+const renderCenterX = (Number(x2) - Number(x1)) / 2
+const renderCenterY = (Number(z2) - Number(z1)) / 2
+
+const areaUrl = `http://localhost:7542/three/area/${x1}/${z1}/${x2}/${z2}`
 const meshUrl = 'http://localhost:7542/three/tiles/17/65529/65539'
 const textureAtlasUrl = 'http://localhost:7542/textureAtlas'
 // const textureAtlasUrl = 'http://localhost:7542/tiles/17/65529/65539?dimension=0.2745372073367526'
@@ -99,25 +98,65 @@ texture.minFilter = THREE.NearestFilter;
 texture.wrapT = THREE.RepeatWrapping;
 // texture.wrapS = THREE.RepeatWrapping;
 
-interface WhyMesh {
+interface WhyObject {
+    type: string;
+    posX: number;
+    posY: number;
+    children?: WhyObject[];
+}
+interface WhyMesh extends WhyObject {
     vertices: number[];
     indices: number[];
     uvs: number[];
 }
 
+
+
 function loadMesh(url: string): Promise<WhyMesh> {
-    return fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            return {
-                vertices: data.vertices,
-                indices: data.faces,
-                uvs: data.uvs
-            }
-        })
+    return fetch(url).then(response => response.json())
 }
 
+function loadObject(url: string): Promise<WhyObject> {
+    return fetch(url).then(response => response.json())
+}
+
+function displayMesh(mesh: WhyMesh, offsetX: number = 0, offsetY: number = 0) {
+    const geometry = new THREE.BufferGeometry();
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(mesh.vertices, 3));
+    geometry.setAttribute('uv', new THREE.Float32BufferAttribute(mesh.uvs, 2));
+    geometry.setIndex(mesh.indices);
+    console.log(mesh.indices.length / 3)
+
+    const material = new THREE.MeshStandardMaterial({map: texture, color: 0xffffff});
+    material.flatShading = true
+    // const material = new THREE.MeshBasicMaterial({map: texture});
+    const object = new THREE.Mesh(geometry, material);
+    object.position.x = offsetX + mesh.posX;
+    object.position.z = offsetY + mesh.posY;
+    object.position.y = -63;
+    scene.add(object);
+    console.log(`object added at ${object.position.x} ${object.position.z}`)
+}
+
+function displayObject(object: WhyObject, offsetX: number = 0, offsetY: number = 0) {
+    if(object.children) {
+        object.children.forEach(child => displayObject(child, offsetX + object.posX, offsetY + object.posY))
+    }
+    if(object.type) {
+        if (object.type === 'dev.wefhy.whymap.tiles.mesh.ThreeJsMesh') {
+            displayMesh(object as WhyMesh, offsetX, offsetY)
+        } else {
+            console.log(object.type)
+        }
+    }
+}
+
+loadObject(areaUrl).then(object => {
+    displayObject(object, -renderCenterX, -renderCenterY)
+})
+
 loadMesh(meshUrl).then(mesh => {
+    return
     const geometry = new THREE.BufferGeometry();
     geometry.setAttribute('position', new THREE.Float32BufferAttribute(mesh.vertices, 3));
     geometry.setAttribute('uv', new THREE.Float32BufferAttribute(mesh.uvs, 2));
@@ -138,39 +177,6 @@ loadMesh(meshUrl).then(mesh => {
     object.castShadow = true;
     object.receiveShadow = true;
     scene.add(object);
-
 })
 
-// const color = 0xFFFFFF;
-// const intensity = 1;
-// const light = new THREE.AmbientLight(color, intensity);
-// scene.add(light);
 
-
-
-
-
-
-// import typescriptLogo from './typescript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.ts'
-//
-// document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://www.typescriptlang.org/" target="_blank">
-//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-//     </a>
-//     <h1>Vite + TypeScript</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite and TypeScript logos to learn more
-//     </p>
-//   </div>
-// `
-//
-// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2023 wefhy
+
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: "./",
+    // your other configuration...
+});
